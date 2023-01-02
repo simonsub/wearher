c05fcbcec476d613753a5d2dbb6a5f0ec21a6aa0
Šimon Šubík <subiksimon@gmail.com>
2023-01-02 20:21:19 +0100

    Updates

---
+++ precache-manifest.d871a16da12c17096fa180e53edec1a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e562df8cc7b5aa2924bc3e384617c069",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "8121bdcc9791ee3cb98e",
+    "url": "/weather/static/css/main.5db5a579.chunk.css"
+  },
+  {
+    "revision": "606df0bd0222acbe31ed",
+    "url": "/weather/static/js/2.5871177d.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/weather/static/js/2.5871177d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8121bdcc9791ee3cb98e",
+    "url": "/weather/static/js/main.df7514b9.chunk.js"
+  },
+  {
+    "revision": "06def81043d555b9b2c7",
+    "url": "/weather/static/js/runtime-main.eba7de3f.js"
+  },
+  {
+    "revision": "f03b5cf758e37656a5cde3eb093130e8",
+    "url": "/weather/static/media/bg.f03b5cf7.png"
+  },
+  {
+    "revision": "634ee2deb2591696fc4b4f13cddc0b68",
+    "url": "/weather/static/media/geoloc.634ee2de.svg"
+  },
+  {
+    "revision": "417c71ed1d95e6bfec4be7a9745d522f",
+    "url": "/weather/static/media/humidity.417c71ed.svg"
+  },
+  {
+    "revision": "0cd81fbf17c8cdaf66f7746b85f117aa",
+    "url": "/weather/static/media/pressure.0cd81fbf.svg"
+  },
+  {
+    "revision": "66b405267cbe11758d9ab82fd04f4a95",
+    "url": "/weather/static/media/search.66b40526.ico"
+  },
+  {
+    "revision": "01467f7c2f879cb03c99b0fd709df90f",
+    "url": "/weather/static/media/wind_speed.01467f7c.svg"
+  }
+]);
